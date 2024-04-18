4f20365f58b4a6a7dca27b8b9f8808c4b284eb66
Yash Srivastava <45912378+Witelightning@users.noreply.github.com>
2024-04-18 15:24:36 -0400

    bookstore app

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://kleayash:kleayash@book-store-mern.uveqojj.mongodb.net/bookstore';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
